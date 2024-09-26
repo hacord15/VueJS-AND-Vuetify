b51a12c3ea36a6d934e5b3e6852befd72b5eaf3a
Abhineet Pal <111232688+hacord15@users.noreply.github.com>
2024-09-26 21:14:24 +0530

    Add files via upload

---
+++ VueJS/app.js
@@ -0,0 +1,15 @@
+// console.log("print")
+// const app = Vue.createApp({
+//     template:'<h2>Hello with Vue CND</h2>'
+// });
+// app.mount("#app")
+
+
+//make a instanse
+new  Vue({
+   el:'#app', // Vue will control the div with id="app"
+   data:{
+    name:'abhi',
+    last:'pal'
+   }
+});
